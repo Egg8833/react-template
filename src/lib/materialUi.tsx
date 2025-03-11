2f10f27048a75a624a7277652a105deaa4d5f8a1
黃禎平 <egg8833@gmail.com>
2025-03-11 16:42:47 +0800

    [feat] 將Material 抽取出來到lib資料夾

---
+++ src/lib/materialUi.tsx
@@ -0,0 +1,63 @@
+// theme.ts
+import { createTheme, ThemeProvider,CssBaseline } from "@mui/material";
+import { blue } from "@mui/material/colors";
+
+declare module '@mui/material/Button' {
+  interface ButtonPropsVariantOverrides {
+    dashed: true;
+  }
+}
+
+ const theme = createTheme({
+  spacing: 4,
+  palette: {
+    primary: {
+      main: "#FF5733",
+    },
+    secondary: {
+      main: "#E0C2FF",
+      contrastText: "#47008F",
+    },
+  },
+  components: {
+    MuiButton: {
+      styleOverrides: {
+        root: {
+          "&:hover": {
+            backgroundColor: "#D84315", // 全局設定 primary hover 顏色
+          },
+          "&.MuiButton-containedSecondary:hover": {
+            backgroundColor: "#A772FF", // 全局設定 secondary hover 顏色
+          },
+        },
+      },
+      variants: [
+        {
+          props: { variant: "outlined" },
+          style: {
+            borderWidth: "3px", // 覆蓋原先的 1px 設定
+          },
+        },
+        {
+          props: { variant: "dashed" }, // `dashed` 是自定義 variant
+          style: {
+            textTransform: "none",
+            border: `2px dashed ${blue[500]}`,
+          },
+        },
+      ],
+    },
+  },
+  typography: {
+    h1: {
+      fontSize: "24px", // 設定 h1 字體大小
+    },
+  },
+});
+
+export const MaterialWrapper = ({ children }: { children: React.ReactNode }) => (
+  <>
+    <CssBaseline />
+    <ThemeProvider theme={theme}>{children}</ThemeProvider>
+  </>
+);
