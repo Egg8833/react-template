4bb7f10b277aed49417f504631813f96e8329920
黃禎平 <egg8833@gmail.com>
2025-03-11 16:17:07 +0800

    [feat] 移除 unocss

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,14 @@ import './App.css'
 import { Button, Typography } from "@mui/material";
 import { useCounterStore } from "./store/counterStore.ts";
 
+
+
 function App() {
  const { count, increment, decrement } = useCounterStore();
 
   return (
     <>
+
       <div>
         <a href="https://vite.dev" target="_blank" className=''>
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -18,8 +21,9 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1 className='text-3xl font-bold text-red underline'>Vite + React</h1>
-       <Typography variant="h5">Count: {count}</Typography>
+      <h1 >Vite + React</h1>
+      <Typography variant="h1">Vite + React TTT</Typography>
+       <Typography variant="h2">Count: {count}</Typography>
       <Button variant="contained" color="primary" onClick={increment} style={{ margin: "10px" }}>
         Increment
       </Button>
@@ -29,6 +33,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+
     </>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,74 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import CssBaseline from '@mui/material/CssBaseline';
 import './index.css'
 import App from './App.tsx'
-import 'virtual:uno.css'
+// import 'virtual:uno.css'
+import { createTheme,ThemeProvider } from "@mui/material";
+import { blue } from "@mui/material/colors";
+declare module '@mui/material/Button' {
+  interface ButtonPropsVariantOverrides {
+    dashed: true;
+  }
+}
+
+
+const theme = createTheme({
+  spacing: 4,
+  palette: {
+    primary: {
+      main: "#FF5733",
+    },
+    secondary: {
+      main: "#E0C2FF",
+      contrastText: "#47008F",
+    },
+  },
+  components: {
+    MuiButton: {
+      styleOverrides: {
+        root: {
+          "&:hover": {
+            backgroundColor: "#D84315", // 全局設定 primary hover 顏色
+          },
+          "&.MuiButton-containedSecondary:hover": {
+            backgroundColor: "#A772FF", // 全局設定 secondary hover 顏色
+          },
+        },
+      },
+      variants: [
+        {
+          props: { variant: "outlined" },
+          style: {
+            borderWidth: "3px", // 覆蓋原先的 1px 設定
+          },
+        },
+        {
+          props: { variant: "dashed" }, // `dashed` 是自定義 variant
+          style: {
+            textTransform: "none",
+            border: `2px dashed ${blue[500]}`,
+          },
+        },
+      ],
+    },
+  },
+   typography:{
+     h1: {
+      fontSize: "24px", // 設定 h1 字體大小
+
+    },
+  }
+});
+
+console.log(theme);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <CssBaseline>
+    <ThemeProvider theme={theme}>
     <App />
+    </ThemeProvider>
+    </CssBaseline>
   </StrictMode>,
 )
