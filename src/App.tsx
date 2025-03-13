e1a511422c5e7b1dc53773bc9572103ff584e453
黃禎平 <egg8833@gmail.com>
2025-03-13 14:39:25 +0800

    [feat] 移除多於程式碼

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router";
-import Layout from "./layout/Layout";
+import Layout from "@/layout/Layout";
 import Home from "@/pages/Home";
 import About from "@/pages/About";
+
 
 export default function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />  {/* ✅ `index` 代表 `/` */}
+          <Route index element={<Home />} />
           <Route path="about" element={<About />} />
         </Route>
       </Routes>
--- src/main.tsx
+++ src/main.tsx
@@ -1,21 +1,17 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import {StrictMode} from 'react'
-import {createRoot} from 'react-dom/client'
+import ReactDOM,{createRoot} from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import {MaterialWrapper} from './lib/materialUi.tsx'
 // import 'virtual:uno.css'
-// import {BrowserRouter} from 'react-router'
-import ReactDOM from "react-dom/client";
 
 const root = document.getElementById('root') as HTMLElement;
 
 ReactDOM.createRoot(root).render(
   <StrictMode>
-
       <MaterialWrapper>
         <App />
       </MaterialWrapper>
-
   </StrictMode>
 )
