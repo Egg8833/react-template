75690014596cbd2dbc349360e89c1bf228dbc308
黃禎平 <egg8833@gmail.com>
2025-03-27 13:53:19 +0800

    [feat] TanstakQuery install

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,20 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import {StrictMode} from 'react'
-import ReactDOM,{createRoot} from 'react-dom/client'
+import ReactDOM, {createRoot} from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import {MaterialWrapper} from './lib/materialUi.tsx'
 import 'virtual:uno.css'
+import {TanstackQuery} from './lib/tanstackQuery.tsx'
 
-const root = document.getElementById('root') as HTMLElement;
+const root = document.getElementById('root') as HTMLElement
 
 ReactDOM.createRoot(root).render(
   <StrictMode>
+    <TanstackQuery>
       <MaterialWrapper>
         <App />
       </MaterialWrapper>
+    </TanstackQuery>
   </StrictMode>
 )
