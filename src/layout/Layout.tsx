014053c0d1e1cccfb6c42785f462f99bdc97c239
黃禎平 <egg8833@gmail.com>
2025-04-30 11:46:22 +0800

    refactor(Layout): 移除不必要的 Box 元件，簡化佈局結構

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,18 +1,17 @@
 import Sidebar from "@/components/SideBar";
 import Header from "@/components/Header";
-import { Box } from "@mui/material";
-import {  Outlet } from "react-router";
+import { Outlet } from "react-router";
 
 export default function Layout() {
   return (
     <>
-    <Header/>
-    <Box sx={{ display: "flex"}}>
-      <Sidebar/>
-      <div className="flex-1 p-4 " >
-      <Outlet />
+      <Header />
+      <div className="flex">
+        <Sidebar />
+        <div className="flex-1 p-4" >
+          <Outlet />
+        </div>
       </div>
-    </Box >
     </>
   );
 }
