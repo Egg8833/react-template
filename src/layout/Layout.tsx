c579e36f27a42ad50ff7aedb5f02ea2666744112
黃禎平 <egg8833@gmail.com>
2025-05-08 08:48:59 +0800

    [fix] 側邊欄調整

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -6,11 +6,11 @@ export default function Layout() {
   return (
     <>
       <Header />
-      <div className="flex ">
-        <div className="bg-#f5f5f5">
+      <div className="flex">
+        <div className="bg-[#f5f5f5] min-h-[calc(100vh-60px)] h-auto">
           <Sidebar  />
         </div>
-        <div className="flex-1 p-4" >
+        <div className="flex-1 p-4">
           <Outlet />
         </div>
       </div>
