fe4c13e137eabc233024d32f538dc670edc27db0
egg8833 <egg8833@gmail.com>
2025-06-03 09:58:50 +0800

    [feat] 調整資料夾內容

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -1,3 +1,4 @@
-export { CustomDataTable } from './CustomDataTable'
-export { MaterialWrapper } from './materialWrapper'
+export { CustomDataTable } from './components/CustomDataTable'
+export { default as CaptchaCanvas } from './components/CaptchaCanvas'
+export { MaterialWrapper } from './lib/materialWrapper'
 export { alertSucc, alertWarn, alertErr, alertText } from './utils/useSwal'
